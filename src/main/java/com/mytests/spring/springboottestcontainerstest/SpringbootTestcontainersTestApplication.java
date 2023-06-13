609103e304e887056a6d2a7887aea8a343b893f2
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2023-06-13 12:13:28 +0200

    persons service

--- src/main/java/com/mytests/spring/springboottestcontainerstest/SpringbootTestcontainersTestApplication.java
+++ src/main/java/com/mytests/spring/springboottestcontainerstest/SpringbootTestcontainersTestApplication.java
@@ -1,13 +1,28 @@
 package com.mytests.spring.springboottestcontainerstest;
 
+import com.mytests.spring.springboottestcontainerstest.services.PersonsService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootTestcontainersTestApplication {
+public class SpringbootTestcontainersTestApplication implements CommandLineRunner {
+
+    private final PersonsService personsService;
+
+    public SpringbootTestcontainersTestApplication(PersonsService personsService) {
+        this.personsService = personsService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootTestcontainersTestApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        personsService.initDB();
+        personsService.displayAll();
+        personsService.updateStatus();
+        personsService.displayAll();
+    }
 }
--- src/main/java/com/mytests/spring/springboottestcontainerstest/services/PersonsService.java
+++ src/main/java/com/mytests/spring/springboottestcontainerstest/services/PersonsService.java
@@ -8,15 +8,32 @@ import org.springframework.stereotype.Service;
 public class PersonsService {
 
 
+   private final PersonRepository personRepository;
 
-    public void initDB(){
-        personRepository.
+   public PersonsService(PersonRepository personRepository) {
+      this.personRepository = personRepository;
+   }
+
+   public void initDB(){
+
        personRepository.save(new Person(1,"vasya","pupkin",25,"default"));
        personRepository.save(new Person(2,"vanya","petrov",15,"default"));
        personRepository.save(new Person(3,"valya","sidorov",15,"default"));
        personRepository.save(new Person(4,"petya","ivanov",30,"default"));
        personRepository.save(new Person(5,"pasha","pavlov",33,"default"));
+       personRepository.save(new Person(6,"pasha","ivanov",70,"default"));
+       personRepository.save(new Person(7,"sasha","pavlova",63,"default"));
 
+   }
+
+   public void displayAll(){
+      for (Person person : personRepository.findAll()) {
+         System.out.println(person);
+      }
+   }
+
+   public void updateStatus(){
+      personRepository.updateStatus("senior", 60);
    }
 
 }
